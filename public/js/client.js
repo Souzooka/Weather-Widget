91e7f3b9dd78c1b08a089e3fdbe5944b40c2115b
Dakota <bazookaharwell@gmail.com>
2017-04-11 09:55:26 -1000

    call API

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,6 @@
 function getData(source, element, eFunc) {
   const oReq = new XMLHttpRequest();
+  console.log(source);
 
   oReq.addEventListener('load', function() {
     const data = JSON.parse(this.responseText);
@@ -14,8 +15,9 @@ function addDataToPage(source, data, element) {
 }
 
 document.querySelector('#btn-weather').addEventListener('click', () => {
-  const zipCode = document.querySelector('#btn-weather').value;
+  const zipCode = document.querySelector('#city-input').value;
+  console.log(zipCode);
   const container = document.querySelector('#contentContainer');
-  getData(`api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=${API_KEY}`,
+  getData(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`,
     container, addDataToPage);
 });
