2ca489ca2693b16d8530c9ee66038a40b4481fa6
Dakota <bazookaharwell@gmail.com>
2017-04-11 09:59:08 -1000

    temperature conversion func

--- public/js/client.js
+++ public/js/client.js
@@ -14,6 +14,15 @@ function addDataToPage(source, data, element) {
   console.log(data);
 }
 
+function convertKelvinToFahrenheit(num) {
+  // convert to celsius
+  num -= 273.15;
+  // convert to fahrenheit
+  num *= 1.8;
+  num += 32;
+  return num;
+}
+
 document.querySelector('#btn-weather').addEventListener('click', () => {
   const zipCode = document.querySelector('#city-input').value;
   console.log(zipCode);
